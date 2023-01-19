9da2e1d70f1354aee319089a7dc5877c55a14b5c
Ruan Caetano <ruansouza_caetano@hotmail.com>
2023-01-19 16:20:40 -0300

    refactor: renames linked list files and methods

---
+++ go-examples/linkedlist/singly.go
@@ -0,0 +1,147 @@
+package linkedlist
+
+type SinglyLinkedListNode[T comparable] struct {
+	Next  *SinglyLinkedListNode[T]
+	Value T
+}
+
+type SinglyLinkedList[T comparable] struct {
+	Head *SinglyLinkedListNode[T]
+}
+
+func NewSinglyLinkedListNode[T comparable](value T) *SinglyLinkedListNode[T] {
+	return &SinglyLinkedListNode[T]{
+		Value: value,
+	}
+}
+
+func NewSinglyLinkedList[T comparable]() *SinglyLinkedList[T] {
+	return &SinglyLinkedList[T]{}
+}
+
+// Prepend
+// O(1)
+func (sll *SinglyLinkedList[T]) Prepend(value T) *SinglyLinkedListNode[T] {
+	newNode := NewSinglyLinkedListNode[T](value)
+	if sll.Head == nil {
+		sll.Head = newNode
+		return newNode
+	}
+
+	newNode.Next = sll.Head
+	sll.Head = newNode
+
+	return newNode
+}
+
+// Append
+// O(n)
+func (sll *SinglyLinkedList[T]) Append(value T) *SinglyLinkedListNode[T] {
+	newNode := NewSinglyLinkedListNode[T](value)
+	if sll.Head == nil {
+		sll.Head = newNode
+		return newNode
+	}
+
+	listNode := sll.Head
+	for {
+		if listNode.Next == nil {
+			listNode.Next = newNode
+			return newNode
+		}
+
+		listNode = listNode.Next
+	}
+}
+
+// Size
+// O(n)
+func (sll *SinglyLinkedList[T]) Size() int {
+	size := 0
+	listNode := sll.Head
+	for listNode != nil {
+		size += 1
+		listNode = listNode.Next
+	}
+	return size
+}
+
+// GetIndex
+// O(n)
+func (sll *SinglyLinkedList[T]) GetIndex(value T) int {
+	index := 0
+	listNode := sll.Head
+	for listNode != nil {
+		if value == listNode.Value {
+			return index
+		}
+
+		index += 1
+		listNode = listNode.Next
+	}
+
+	return -1
+}
+
+// GetNodeAt
+// O(n)
+func (sll *SinglyLinkedList[T]) GetNodeAt(desiredIndex int) *SinglyLinkedListNode[T] {
+	index := 0
+	listNode := sll.Head
+	for listNode != nil {
+		if index == desiredIndex {
+			return listNode
+		}
+
+		index += 1
+		listNode = listNode.Next
+	}
+
+	return nil
+}
+
+// DeleteValue
+// O(n)
+func (sll *SinglyLinkedList[T]) DeleteValue(value T) *SinglyLinkedListNode[T] {
+	var nextNode *SinglyLinkedListNode[T]
+	listNode := sll.Head
+	for listNode != nil {
+		if value == listNode.Value {
+			if nextNode == nil {
+				sll.Head = listNode.Next
+				return listNode
+			}
+			nextNode.Next = listNode.Next
+			return listNode
+		}
+
+		nextNode = listNode
+		listNode = listNode.Next
+	}
+
+	return nil
+}
+
+// DeleteValueAt
+// O(n)
+func (sll *SinglyLinkedList[T]) DeleteValueAt(desiredIndex int) *SinglyLinkedListNode[T] {
+	index := 0
+	var nextNode *SinglyLinkedListNode[T]
+	listNode := sll.Head
+	for listNode != nil {
+		if index == desiredIndex {
+			if nextNode == nil {
+				sll.Head = listNode.Next
+				return listNode
+			}
+			nextNode.Next = listNode.Next
+			return listNode
+		}
+
+		nextNode = listNode
+		listNode = listNode.Next
+		index += 1
+	}
+
+	return nil
+}
--- go-examples/linkedlist/singly_test.go
+++ go-examples/linkedlist/singly_test.go
@@ -6,99 +6,99 @@ import (
 	"testing"
 )
 
-func TestNewSimpleLinkedListNode(t *testing.T) {
-	node1 := linkedlist.NewSimpleLinkedListNode[int](1)
+func TestNewSinglyLinkedListNode(t *testing.T) {
+	node1 := linkedlist.NewSinglyLinkedListNode[int](1)
 
 	assert.NotNil(t, node1)
 	assert.Equal(t, 1, node1.Value)
 }
 
-func TestNewSimpleLinkedList(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 
 	assert.NotNil(t, list)
 	assert.Nil(t, list.Head)
 }
 
-func TestNewSimpleLinkedList_InsertAtStart(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_Prepend(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	node1 := list.InsertAtStart(1)
+	node1 := list.Prepend(1)
 	assert.NotNil(t, node1)
 	assert.Nil(t, node1.Next)
 	assert.Equal(t, 1, node1.Value)
 	assert.Equal(t, list.Head, node1)
 
-	node2 := list.InsertAtStart(2)
+	node2 := list.Prepend(2)
 	assert.NotNil(t, node2)
 	assert.Equal(t, node1, node2.Next)
 	assert.Equal(t, 2, node2.Value)
 	assert.Equal(t, list.Head, node2)
 }
 
-func TestNewSimpleLinkedList_InsertAtEnd(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_Append(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	node1 := list.InsertAtEnd(1)
+	node1 := list.Append(1)
 	assert.NotNil(t, node1)
 	assert.Nil(t, node1.Next)
 	assert.Equal(t, 1, node1.Value)
 
-	node2 := list.InsertAtEnd(2)
+	node2 := list.Append(2)
 	assert.NotNil(t, node2)
 	assert.Nil(t, node2.Next)
 	assert.Equal(t, node2, node1.Next)
 	assert.Equal(t, 2, node2.Value)
 
-	node3 := list.InsertAtEnd(3)
+	node3 := list.Append(3)
 	assert.NotNil(t, node3)
 	assert.Nil(t, node3.Next)
 	assert.Equal(t, node3, node2.Next)
 	assert.Equal(t, 3, node3.Value)
 }
 
-func TestNewSimpleLinkedList_Size(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_Size(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	list.InsertAtStart(1)
-	list.InsertAtStart(2)
+	list.Prepend(1)
+	list.Prepend(2)
 
 	assert.Equal(t, 2, list.Size())
 }
 
-func TestNewSimpleLinkedList_GetIndex(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_GetIndex(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	list.InsertAtEnd(1)
-	list.InsertAtEnd(2)
+	list.Append(1)
+	list.Append(2)
 
 	assert.Equal(t, 0, list.GetIndex(1))
 	assert.Equal(t, 1, list.GetIndex(2))
 	assert.Equal(t, -1, list.GetIndex(3))
 }
 
-func TestNewSimpleLinkedList_GetValueAt(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_GetValueAt(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	node1 := list.InsertAtEnd(1)
-	node2 := list.InsertAtEnd(2)
+	node1 := list.Append(1)
+	node2 := list.Append(2)
 
 	assert.Equal(t, node1, list.GetNodeAt(0))
 	assert.Equal(t, node2, list.GetNodeAt(1))
 	assert.Nil(t, list.GetNodeAt(2))
 }
 
-func TestNewSimpleLinkedList_DeleteValue(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_DeleteValue(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	node1 := list.InsertAtEnd(1)
-	node2 := list.InsertAtEnd(2)
+	node1 := list.Append(1)
+	node2 := list.Append(2)
 
 	assert.Equal(t, 2, list.Size())
 	assert.Equal(t, node2, list.DeleteValue(2))
@@ -106,14 +106,15 @@ func TestNewSimpleLinkedList_DeleteValue(t *testing.T) {
 	assert.Equal(t, node1, list.DeleteValue(1))
 	assert.Equal(t, 0, list.Size())
 	assert.Nil(t, list.DeleteValue(0))
+	assert.Nil(t, list.Head)
 }
 
-func TestNewSimpleLinkedList_DeleteValueAt(t *testing.T) {
-	list := linkedlist.NewSimpleLinkedList[int]()
+func TestNewSinglyLinkedList_DeleteValueAt(t *testing.T) {
+	list := linkedlist.NewSinglyLinkedList[int]()
 	assert.NotNil(t, list)
 
-	node1 := list.InsertAtEnd(1)
-	node2 := list.InsertAtEnd(2)
+	node1 := list.Append(1)
+	node2 := list.Append(2)
 
 	assert.Equal(t, 2, list.Size())
 	assert.Equal(t, node2, list.DeleteValueAt(1))
@@ -121,4 +122,5 @@ func TestNewSimpleLinkedList_DeleteValueAt(t *testing.T) {
 	assert.Equal(t, node1, list.DeleteValueAt(0))
 	assert.Equal(t, 0, list.Size())
 	assert.Nil(t, list.DeleteValueAt(0))
+	assert.Nil(t, list.Head)
 }
